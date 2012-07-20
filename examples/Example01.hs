38e84cef6e54d94563ce33bce1d226104e118fe4
Nils Schweinsberg <mail@n-sch.de>
2012-07-20 17:46:07 +0200

    small example

--- examples/Example01.hs
+++ examples/Example01.hs
@@ -1,14 +1,23 @@
-module Main where
-
 import Data.IGraph
+
+{-
+
+Graph:
+
+  0 --- 1 --- 2 --- 3 --- 4 --- 5
+   \               / \         /
+    `-------------´   `-------´
+
+-}
+
+g :: Graph U Int
+g = fromList $ [(0,1), (1,2), (2,3), (3,4), (4,5), (0,3), (3,5)]
 
 main :: IO ()
 main = do
-  let gr = make [("1","2"),("2","1"),("3","2"),("4","5")]
-  print (nodeNumber gr)
-  print (edgeNumber gr)
-  print (betweenness  gr)
-  print (eigenvectorCentrality  gr)
-  print (cluster gr)
-  print (shortestPathsIn gr "1")
-  return ()
+  putStrLn $ "Nodes:                     " ++ show (nodes g)
+  putStrLn $ "Edges:                     " ++ show (edges g)
+  putStrLn $ "0 and 3 are connected:     " ++ show (areConnected g 0 3)
+  putStrLn $ "0 and 5 are connected:     " ++ show (areConnected g 0 5)
+  putStrLn $ "Graph is connected:        " ++ show (isConnected g Weak)
+  putStrLn $ "Shortest path from 0 to 5: " ++ show (getShortestPath g 0 5 Out)
