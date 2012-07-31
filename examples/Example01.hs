335dbf775ca4c4cb8f8d901c83442e2e8d659b1f
Nils Schweinsberg <mail@n-sch.de>
2012-07-31 19:13:06 +0200

    update example

--- examples/Example01.hs
+++ examples/Example01.hs
@@ -10,14 +10,14 @@ Graph:
 
 -}
 
-g :: Graph U Int
+g :: Graph D Int
 g = fromList $ [(0,1), (1,2), (2,3), (3,4), (4,5), (0,3), (3,5)]
 
 main :: IO ()
 main = do
-  putStrLn $ "Nodes:                     " ++ show (nodes g)
-  putStrLn $ "Edges:                     " ++ show (edges g)
-  putStrLn $ "0 and 3 are connected:     " ++ show (areConnected g 0 3)
-  putStrLn $ "0 and 5 are connected:     " ++ show (areConnected g 0 5)
-  putStrLn $ "Graph is connected:        " ++ show (isConnected g Weak)
-  putStrLn $ "Shortest path from 0 to 5: " ++ show (getShortestPath g 0 5 Out)
+  putStrLn $ "Nodes:                     " ++ show (evalIGraph g $ nodes)
+  putStrLn $ "Edges:                     " ++ show (evalIGraph g $ edges)
+  putStrLn $ "0 and 3 are connected:     " ++ show (evalIGraph g $ areConnected 0 3)
+  putStrLn $ "0 and 5 are connected:     " ++ show (evalIGraph g $ areConnected 0 5)
+  putStrLn $ "Graph is connected:        " ++ show (evalIGraph g $ isConnected Weak)
+  putStrLn $ "Shortest path from 0 to 5: " ++ show (evalIGraph g $ getShortestPath 0 5 Out)
