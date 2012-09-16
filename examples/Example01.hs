580f5c28643018d9a296c7e886451314e614d587
Nils Schweinsberg <mail@n-sch.de>
2012-09-16 17:04:24 +0200

    small changes to the example file

--- examples/Example01.hs
+++ examples/Example01.hs
@@ -2,16 +2,21 @@ import Data.IGraph
 
 {-
 
-Graph:
+Graphs:
 
-  0 --- 1 --- 2 --- 3 --- 4 --- 5
-   \               / \         /
-    `-------------´   `-------´
+g: 0 --- 1 --- 2 --- 3 --- 4 --- 5
+    \               / \         /
+     `-------------´   `-------´
+
+h: 0 --- 1 --- 2 --- 3   4 --- 5 --- 6
+    \               /     \         /
+     `-------------´       `-------´
 
 -}
 
-g :: Graph D Int
+g,h :: Graph D Int
 g = fromList [(0,1), (1,2), (2,3), (3,4), (4,5), (0,3), (3,5)]
+h = fromList [(0,1), (1,2), (2,3), (0,3), (4,5), (5,6), (4,6)]
 
 main :: IO ()
 main = do
@@ -20,5 +25,5 @@ main = do
   putStrLn $ "0 and 3 are connected:       " ++ show (areConnected g 0 3)
   putStrLn $ "0 and 5 are connected:       " ++ show (areConnected g 0 5)
   putStrLn $ "Graph is connected:          " ++ show (isConnected g Weak)
-  -- putStrLn $ "Shortest path from 0 to 5:   " ++ show (getShortestPath 0 5 Out)
+  putStrLn $ "Shortest path from 0 to 5:   " ++ show (getShortestPath g 0 5 Out)
   -- putStrLn $ "Subgraph containing [3,4,5]: " ++ show (subgraph (VsList [3,4,5]))
