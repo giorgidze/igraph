a6b599f98f3c6d88cfb2147446ffe892b555f48e
Nils Schweinsberg <mail@nils.cc>
2012-12-12 16:40:57 +0100

    some examples for pagerank, subgraphs and decomposed graphs etc

--- examples/Example01.hs
+++ examples/Example01.hs
@@ -1,4 +1,5 @@
 import Data.IGraph
+import Data.List
 
 {-
 
@@ -12,6 +13,8 @@ h: 0 --- 1 --- 2 --- 3   4 --- 5 --- 6
     \               /     \         /
      `-------------´       `-------´
 
+           .----2---.
+          /          \
 w: 0 -1- 1 -3- 2 -1- 3
     \               /
      `------1------´
@@ -23,14 +26,29 @@ g = fromList [(0,1), (1,2), (2,3), (3,4), (4,5), (0,3), (3,5)]
 h = fromList [(0,1), (1,2), (2,3), (0,3), (4,5), (5,6), (4,6)]
 
 w :: Graph (Weighted D) Int
-w = fromListWeighted [ (0,1, 1), (1,2, 3), (1,3, 1), (3,2, 1) ]
+w = fromListWeighted [ (0,1, 1), (1,2, 3), (1,3, 1), (3,2, 1), (1,3, 2) ]
+
+(~>) :: a -> b -> (a,b)
+a ~> b = (a,b)
 
 main :: IO ()
 main = do
-  putStrLn $ "Nodes:                       " ++ show (nodes g)
-  putStrLn $ "Edges:                       " ++ show (edges g)
-  putStrLn $ "0 and 3 are connected:       " ++ show (areConnected g 0 3)
-  putStrLn $ "0 and 5 are connected:       " ++ show (areConnected g 0 5)
-  putStrLn $ "Graph is connected:          " ++ show (isConnected g Weak)
-  putStrLn $ "Shortest path from 0 to 5:   " ++ show (getShortestPath g 0 5)
-  putStrLn $ "Subgraph containing [3,4,5]: " ++ show (inducedSubgraph g (VsList [3,4,5]) SubgraphAuto)
+  putStrLn $ "Nodes:\n\t"                       ++ show (nodes g)
+  putStrLn $ "Edges:\n\t"                       ++ show (edges g)
+  putStrLn $ "0 and 3 are connected:\n\t"       ++ show (areConnected g 0 3)
+  putStrLn $ "0 and 5 are connected:\n\t"       ++ show (areConnected g 0 5)
+  putStrLn $ "Graph is connected:\n\t"          ++ show (isConnected g Weak)
+  putStrLn $ "Shortest path from 0 to 5:\n\t"   ++ show (getShortestPath g 0 5)
+  putStrLn $ "Subgraph containing [3,4,5]:\n\t" ++ show (inducedSubgraph g (VsList [3,4,5]) SubgraphAuto)
+  putStrLn $ "h decomposed (weakly):\n\t"       ++ intercalate "\n\t" (map show (decompose h Weak (-1) 0))
+
+pagerankExample :: (Double, [(Char, Double)])
+pagerankExample = pagerank graph VsAll 0.85
+ where
+  graph :: Graph D Char
+  graph = fromList
+        [ 'A' ~> 'B'
+        , 'A' ~> 'C'
+        , 'B' ~> 'C'
+        , 'C' ~> 'A'
+        , 'D' ~> 'C' ]
